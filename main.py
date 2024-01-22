e6b6b98cccbcded2e401f632185af113b3a4d270
KarolineH <karoline@heiwolt.de>
2024-01-22 20:27:51 +0000

    added class structure, calibration methods now return the same variables, added code to confirm coordinate frame conventions

--- main.py
+++ main.py
@@ -3,15 +3,24 @@ import apriltag
 import os
 import numpy as np
 import yaml
-from colmap_wrapper.llff.poses.pose_utils import gen_poses, load_data
+from colmap_wrapper.llff.poses.pose_utils import gen_poses, load_data, load_camera_poses
 from colmap_wrapper.llff.poses.colmap_read_model import read_cameras_binary
 
 
 class CameraCalibration():
-    def __init__(self) -> None:
+
+    # shared between all instances of the class
+
+
+    def __init__(self, cam_id, base_dir=None):
+        self.cam_id = cam_id
+        self.resolution = None
+        self.base_dir = base_dir
+        self.matrix = None
+        self.distortion = None 
         pass
 
-    def init_calibrate(self, cameras, filename=None):
+    def init_calibrate(self):
         # Estimate intrinsics and extrinsics for all cameras
         # write to file if filename is provided
         # in real-world physical units
@@ -64,27 +73,40 @@ class CameraCalibration():
         # more details here https://colmap.github.io/format.html#output-format
         # and here https://colmap.github.io/faq.html#reconstruct-sparse-dense-model-from-known-camera-poses
 
-    def colmap_calibration(self, in_dir):
+    def colmap_calibration(self):
         """
-        Calibrate a single camera using multiple images taken of the same scene.
-        :param in_dir: The base directory containing a subfolder 'images'.
-        :return: ...
+        Calibrate the camera using a provided series of calibration images.
+        Will use the images in base_dir/images/ for calibration.
+        Currently assumes sequential image series and the OpenCV camera model.
+        Does not take any known camera poses, so the reference frame of the reconstructed model is NOT anchored to the real world frame.
         """
+        # First run COLMAP to get all camera parameters
+        # Options:
+            # OPENCV model yields fx, fy, cx, cy, k1, k2, p1, p2
+            # SIMPLE_RADIAL yields f, cx, cy, k
+            # exhaustive_matcher or sequential_matcher
+            # If a sparse reconstruction model already exist in the folder, it does not run again. But you can force it to run again by setting force_rerun=True.
+        gen_poses(basedir=self.base_dir, match_type='sequential_matcher', model_type='OPENCV', force_rerun=False)
 
-        # The input directory should contain a folder 'images' with all images used for calibration.
-        # OPENCV model yields fx, fy, cx, cy, k1, k2, p1, p2
-        # SIMPLE_RADIAL yields f, cx, cy, k
-        gen_poses(basedir=in_dir, match_type='sequential_matcher', model_type='OPENCV') # exhaustive_matcher or sequential_matcher
-
-        # Now retrieve the camera parameters from the colmap output
-        camerasfile = os.path.join(in_dir, 'sparse/0/cameras.bin')
+        # Retrieve the camera parameters from the colmap output
+        camerasfile = os.path.join(self.base_dir, 'sparse/0/cameras.bin')
         camdata = read_cameras_binary(camerasfile)
         list_of_keys = list(camdata.keys())
-        cam = camdata[list_of_keys[0]] 
+        cam = camdata[list_of_keys[0]]
+        resolution = np.array([cam.width, cam.height]) # width, height
+        mtx = np.array([[cam.params[0],0,cam.params[2]],[0,cam.params[1],cam.params[3]],[0,0,1]])
+        dist = np.array(cam.params[4:])
 
-        # Now retrieve the camera poses from the colmap output
-        poses,_,_ = load_data(in_dir)
-        return cam, poses
+        # Retrieve the camera poses from the colmap output
+        cam_in_world = load_camera_poses(self.base_dir) #c2w: camera to world transformation matrices for each frame
+        # these assume a frame only specified as [r, -u, t], which we'll assume is x-right, y-down, z-through the image plane] (bc colmap uses right handed frames)
+
+        # poses,_ = load_data(self.base_dir, load_imgs=False)
+        # from the regular COLMAP output orientation, this is rotated so that the targeted frame is: x-down, y-right, and x-backwards/towards the camera.
+        # Each poses[:,:-1,0] is a 3x4 homogeneous transformation matrix, with the last row left out (because it is always [0,0,0,1])
+        # w2c_mats and c2w_mats are avaliable also in pose_utils
+
+        return resolution, mtx, dist, cam_in_world
     
     def april_tag_cam_position(self, in_dir):
         """
@@ -94,34 +116,40 @@ class CameraCalibration():
         """
         return
 
-    def april_tag_calibration(self, in_dir):
+    def april_tag_calibration(self, obj_in_world=np.eye(4)):
         """
-        Calibrate a single camera using multiple images of a pre-made pattern of AprilTags.
-        :param in_dir: The base directory for this calibration operation, containing a subfolder called 'images' holding the collection of images used for calibration.
+        Calibrate the camera using a pre-made pattern of AprilTags visible in a series of calibration images.
+        Assumes that all images were taken with the same camera, at the same resolution and focal length, and have not been rotated.
         :return: dict of intrinics (incl. RMS re-projection error, camera matrix, distortion coefficients), estimated rotation vectors and translation vectors for all provided images
         """
+        # cv2 (opencv) uses a right-handed coordinate system with positive axes going x-right, y-down, z-forward (away from camera through the image plane)
+        # We use the same convention throughout this function.
+
+        # The pose of the pattern in the world frame (obj_in_world) must be known. It is assumed to be the identity matrix by default. 
+        # So unless specified, the origin of the pattern is also the origin of the world frame.
+
+        # create the detector
+        '''APRILTAG pattern specifications'''
         # These parameters are specific to the printed calibration pattern used.
         # The IDs of the tags are laid out in a grid pattern, so that the exact location can always be determined.
         tag_family = "tag36h11" # All tags in the pattern are of the same tag family to distinguish them from other tags in the environment.
         tag_spacing = 0.01  # in meters
         tag_size = 0.03 # in meters
         tag_layout = np.array([4,6]) # rows x columns
-        spacing = tag_spacing + tag_size
-
-        # create the detector
         options = apriltag.DetectorOptions(families=tag_family)
         detector = apriltag.Detector(options)
-
-        # recreate the calibration pattern in 'world' coordinates
-        # starting at the upper left corner of the pattern (origin) going first right in x direction, then down in y direction
+        # recreate the calibration pattern in 'world coordinates' (opencv), this means in real world units but given in the pattern's local frame
+        # starting at the upper left corner of the pattern (origin) going first left to right in x direction, then down in y direction, z is always 0
+        spacing = tag_spacing + tag_size
         corner_array = np.array([[i * spacing, j * spacing, 0] for j in range(tag_layout[0]) for i in range(tag_layout[1])], dtype=np.float32)
 
         obj_points = []
         img_points = []
-        im_dir = in_dir + "images/"
+        image_directory = self.base_dir + "images/"
 
-        for image in os.listdir(im_dir):
-            img = cv2.imread(im_dir + image)
+        # Detect tags in all provided images
+        for image in os.listdir(image_directory):
+            img = cv2.imread(image_directory + image) # row, column
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             # detect all tags in the image
             # if only part of the pattern is visible, we still use all available tags
@@ -132,12 +160,45 @@ class CameraCalibration():
 
             obj_points.append(world_coords)
             img_points.append(image_coords)
+        resolution = gray.shape[::-1] # width, height 
 
         # Calibrate the camera based on all detected tags in all provided images
         # For this initial calibration no cmaera matrix or distortion coefficients are provided
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, gray.shape[::-1], None, None) # arguments are object points, image points, image size, camera matrix, distortion coefficients
-        # the extrinsics are the rotation and translation vectors bring the pattern from object frame to camera frame, same as the position of the pattern in the camera frame
-        return ret, mtx, dist, rvecs, tvecs # RMS re-projection error, camera matrix, distortion coefficients, rotation vectors, translation vectors 
+        print("RMS re-projection error: ", ret)
+
+        # Printing the pattern origin frame onto the images to check
+        # plot = False
+        # if plot:     
+            # img = cv2.imread(image_directory + os.listdir(image_directory)[-4]) # row, column
+            # im2 = cv2.drawFrameAxes(img, mtx, dist, rvecs[-4], tvecs[-4], 0.1, 6)
+            # # rescale the image to fit on screen
+            # scale_percent = 15 # percent of original size
+            # width = int(im2.shape[1] * scale_percent / 100)
+            # height = int(im2.shape[0] * scale_percent / 100)
+            # dim = (width, height)
+            # im2 = cv2.resize(im2, dim, interpolation = cv2.INTER_AREA)
+
+            # cv2.imshow("Image", im2)
+            # cv2.waitKey(0)
+
+        homogeneous_transforms = []
+        # the rotation and translation vectors bring the pattern from object frame to camera frame, that's the same as the pose of the pattern origin given in the camera frame
+        for rvec, tvec in zip(rvecs, tvecs):
+            # Convert rotation vector to rotation matrix using Rodrigues formula
+            rotation_matrix, _ = cv2.Rodrigues(rvec)
+
+            homogeneous_transform = np.eye(4)
+            homogeneous_transform[:3, :3] = rotation_matrix
+            homogeneous_transform[:3, 3] = tvec.flatten()
+            homogeneous_transforms.append(homogeneous_transform)
+
+        obj_in_cam = np.array(homogeneous_transforms) # shape: (n_images, 4, 4), transformation matrices describing object location in the camera frame
+        cam_in_obj = np.linalg.inv(obj_in_cam) # camera poses relative to the pattern origin
+        cam_in_world = np.matmul(cam_in_obj,obj_in_world) # camera poses relative to the world frame
+        # NEED TO KNOW THE POSITION OF THE PATTERN IN THE WORLD FRAME
+
+        return resolution, mtx, dist, cam_in_world # camera matrix, distortion coefficients, 4x4 homogeneous transforms of the camera poses in world frame
     
     def write_to_file(self, file_path, cam_ids, matrices, distortion_params):
         """
@@ -175,14 +236,16 @@ class CameraCalibration():
 
 if __name__ == "__main__":
 
-    cc = CameraCalibration()
-    #rp_error, intrinsic_matrix, distortion_coeff, rvecs, tvecs = cc.april_tag_calibration(in_dir = "/home/karo/rosws/src/camera_calibration/images/")
-
-    cc.write_to_file("test.yaml", np.array([0,1]), [np.array([[1,2,3],[4,5,6],[7,8,9]]), np.array([[1,2,3],[4,5,6],[7,8,9]])], [np.array([1,2,3,4,5]), np.array([1,2,3,4,5])])
-    out = cc.read_from_file("test.yaml")
-    print(out)
-    #cc.april_tag_calibration(in_dir="/home/kh790/rosws/src/camera_calibration/tag_dir/")
-    #cc.colmap_calibration(in_dir="/home/kh790/Desktop/")
+    cc = CameraCalibration(cam_id=0, base_dir="/home/karo/rosws/src/camera_calibration/")
+    cc.april_tag_calibration()
+    # cc = CameraCalibration(cam_id=0, base_dir="/home/karo/Desktop/calibration_test/")
+    # cc.colmap_calibration()
+    
+    # cc.write_to_file("test.yaml", np.array([0,1]), [np.array([[1,2,3],[4,5,6],[7,8,9]]), np.array([[1,2,3],[4,5,6],[7,8,9]])], [np.array([1,2,3,4,5]), np.array([1,2,3,4,5])])
+    # out = cc.read_from_file("test.yaml")
+    # print(out)
+    #cc.april_tag_calibration()
+    cc.colmap_calibration()
     #cc.colmap_calibration(in_dir = "/home/kh790/rosws/src/camera_calibration/scenedir/")
 
 
